5ccefcc91fd9144202ac4ad59007314a6adc5a82
ShahabBngsh <shahabbng1999@gmail.com>
2021-04-11 17:12:04 +0500

    I've removed table dummy data from searchroom.html file. I've passed list of dictionary from main.py file NOTE: this work is temporary, and will be replaced with data       from db table, once our database is created.

--- main.py
+++ main.py
@@ -2,6 +2,58 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+availRooms = [
+    {
+        "roomNo": 101,
+        "capacity": 4,
+        "rate": 15000,
+        "floorNo": 1
+    },
+
+    {
+        "roomNo": 102,
+        "capacity": 3,
+        "rate": 16000,
+        "floorNo": 1
+    },
+
+    {
+        "roomNo": 103,
+        "capacity": 3,
+        "rate": 16000,
+        "floorNo": 1
+    },
+
+    {
+        "roomNo": 201,
+        "capacity": 4,
+        "rate": 15000,
+        "floorNo": 2
+    },
+
+    {
+        "roomNo": 202,
+        "capacity": 3,
+        "rate": 16000,
+        "floorNo": 2
+    },
+
+    {
+        "roomNo": 203,
+        "capacity": 3,
+        "rate": 16000,
+        "floorNo": 2
+    },
+
+    {
+        "roomNo": 301,
+        "capacity": 2,
+        "rate": 17000,
+        "floorNo": 2
+    }
+]
+
+
 #route to login.html page
 #NOTE: if you want to change function name, then also change in template.html file
 
@@ -15,7 +67,7 @@ def signup():
 
 @app.route("/searchroom")
 def searchroom():
-    return render_template("searchroom.html")
+    return render_template("searchroom.html", availRooms=availRooms)
 
 
 if __name__ == "__main__":
