13be6c99c7bfb56afd1de36417ee1c01a8ee925b
ShahabBngsh <shahabbng1999@gmail.com>
2021-04-23 22:23:54 +0500

    Integrated Business Layer code provided by @Piyush

--- main.py
+++ main.py
@@ -53,6 +53,94 @@ availRooms = [
     }
 ]
 
+#Business Layer code by 'Piyush'
+class users:
+    def __init__(self, name, password, cnic, contact_no):
+        self.name = name
+        self.password = password
+        self.cnic = cnic
+        self.contact_no = contact_no
+
+
+class room:
+    def __init__(self, room_no, rate, capacity, tenants):
+        self.room_no = room_no
+        self.rate = rate
+        self.capacity = capacity
+        self.tenants = tenants
+
+
+class room_manager:
+    def __init__(self):
+        self.room_list = []
+
+    def search_rooms(self):
+        pass
+        # self.room_list = DB.search_rooms
+        # return self.room_list
+
+
+class package:
+    def __init__(self, price, description):
+        self.price = price
+        self.description = description
+
+
+class package_manager:
+    def __init__(self):
+        self.package_list = []
+
+    def search_packages(self):
+        pass
+        #self.room_list = DB.search_rooms
+        #return self.room_list
+
+class room_package:
+    def __init__(self, room, package):
+        self.room = room
+        self.package = package
+
+
+class room_package_manager:
+    def __init__(self):
+        self.room_package_list = []
+
+    def reserve_room_and_package(self, room, package):
+        pass
+        #self.room_package_list.append(room_package(room, package))
+        #return DB.reserve_room_and_package()
+
+
+
+class hostel():
+    def __init__(self, address, phone_no, total_rooms):
+        self.address = address
+        self.phone_no = phone_no
+        self.total_rooms = total_rooms
+        self.room_list = []
+        self.PM = package_manager()
+        self.RM = room_manager()
+        self.RPM = room_package_manager()
+
+    def reserve_room_and_package(self, room, package):
+        return self.RPM.reserve_room_and_package(room, package)
+
+    def search_rooms(self):
+
+        return self.RM.search_rooms()
+
+    def search_packages(self):
+
+        return self.PM.search_packages()
+
+class invoice:
+    def __init__(self, fee_total, fee_status):
+        self.fee_total = fee_total
+        self.fee_status = fee_status
+
+class invoice_manager:
+    def __init__(self):
+        self.invoice_list = []
 
 #route to login.html page
 #NOTE: if you want to change function name, then also change in template.html file
